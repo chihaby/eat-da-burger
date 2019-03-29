463b88ee3a6d23f0879f6cd6e95291cbd15832d0
Radouane <38984550+Radouane@users.noreply.github.com>
2019-03-29 06:34:10 -0700

    does not add data

--- server.js
+++ server.js
@@ -16,6 +16,7 @@ var exphbs = require("express-handlebars");
 
 app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
+app.use(express.static(__dirname + '/public'));
 
 // Import routes and give the server access to them.
 var routes = require("./controllers/burgers_Controller.js");
